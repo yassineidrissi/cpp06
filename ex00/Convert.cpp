f472fd350b7e72e1f49a45820995d9406032ebff
Yassine Idrissi <yaidriss@c2r1p9.1337.ma>
2024-01-04 22:32:49 +0100

    merge all function in one function

--- ex00/Convert.cpp
+++ ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 21:17:24 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/01/04 22:21:39 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/01/04 22:32:18 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,34 +59,22 @@ void printf_fd(std::string s)
 	std::cout << YELLOW << "double :" << RESET << f << std::endl;
 }
 
-void convert_int(std::string n)
-{
+// void convert_int(std::string n)
+// {
 
-	double d = static_cast<double>(std::atof(n.c_str()));
-	if (d > INT_MAX || d < INT_MIN)
-	{
-		convert_err();
-		return ;
-	}
-	put_char(d);
-	std::cout << YELLOW << "int : " << RESET << static_cast<int>(d)  << std::endl;
-	printf_fd(n);
-}
+// 	double d = static_cast<double>(std::atof(n.c_str()));
+// 	if (d > INT_MAX || d < INT_MIN)
+// 	{
+// 		convert_err();
+// 		return ;
+// 	}
+// 	put_char(d);
+// 	std::cout << YELLOW << "int : " << RESET << static_cast<int>(d)  << std::endl;
+// 	printf_fd(n);
+// }
 
 
-void convert_d(std::string d)
-{
-	const char * s = d.c_str();	
-	double fd = static_cast<double>(std::atof(s));
-	put_char(static_cast<int>(std::atof(s)));
-	if (fd > INT_MAX || fd < INT_MIN)
-		std::cout << RED << " int : " << RESET << "impossible" << std::endl;
-	else 
-		std::cout << YELLOW << "int : " << RESET << static_cast<int>(std::atof(s)) << std::endl;
-	printf_fd(s);
-}
-
-void convert_f(std::string d)
+void convert_print(std::string d)
 {
 	const char * s = d.c_str();	
 	double fd = static_cast<double>(std::atof(s));
@@ -97,6 +85,18 @@ void convert_f(std::string d)
 		std::cout << YELLOW << "int : " << RESET << static_cast<int>(std::atof(s)) << std::endl;
 	printf_fd(s);
 }
+
+// void convert_f(std::string d)
+// {
+// 	const char * s = d.c_str();	
+// 	double fd = static_cast<double>(std::atof(s));
+// 	put_char(static_cast<int>(std::atof(s)));
+// 	if (fd > INT_MAX || fd < INT_MIN)
+// 		std::cout << RED << " int : " << RESET << "impossible" << std::endl;
+// 	else 
+// 		std::cout << YELLOW << "int : " << RESET << static_cast<int>(std::atof(s)) << std::endl;
+// 	printf_fd(s);
+// }
 
 
 
@@ -161,13 +161,13 @@ void ScalarConverter::convert(std::string s)
 	if(s == "nan" || s == "nanf" || s == "+inf" || s == "+inff" || s == "-inf" || s == "-inff")
 		convert_handl(s);
 	else if((d == len) || (d = len -1 && (s[0] == '-' || s[0] == '+')))
-		convert_int(s);
+		convert_print(s);
 	else if(p < 2 && !f && !err)
-		convert_d(s);
+		convert_print(s);
 	else if(p < 2 && f < 2 && !err && s[len - 1] == 'f')
-		convert_f(s);
+		convert_print(s);
 	else if(i == 1 && std::isdigit(s[0]))
-		convert_int(s);
+		convert_print(s);
 	else
 		convert_err();
 };
